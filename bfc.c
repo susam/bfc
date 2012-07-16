edf2c864932b402d2c78eacae23befbd7b8d1cbd
Susam Pal <susam@susam.in>
2012-07-16 23:15:30 +0530

    Use Simplified BSD (2-clause) license
    Currently this software is available under the terms of the BSD 3-clause
    license. Change the copyright information in the source code, man page
    and accompanying text files to make this software available under the
    terms of the BSD 2-clause license.

--- bfc.c
+++ bfc.c
@@ -4,30 +4,30 @@
  * Copyright (c) 2008 Susam Pal
  * All rights reserved.
  * 
+ * Copyright (c) 2008 Susam Pal
+ * All rights reserved.
+ * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
  *     1. Redistributions of source code must retain the above copyright
  *        notice, this list of conditions and the following disclaimer.
- *     2. Redistributions in binary form must reproduce the above copyright
- *        notice, this list of conditions and the following disclaimer in
- *        the documentation and/or other materials provided with the
- *        distribution.
- *     3. Neither the name of the project nor the names of its contributors
- *        may be used to endorse or promote products derived from this
- *        software without specific prior written permission.
+ *     2. Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials provided
+ *        with the distribution.
  * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
- * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
  * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
- * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #include <stdio.h>
@@ -39,10 +39,10 @@
 #define COPYRIGHT   "Copyright (c) 2008 Susam Pal"
 #define AUTHOR      "Susam Pal"
 #define LICENSE \
-    "This is free software. You are permitted to redistribute and use it\n" \
-    "in source and binary forms, with or without modification, under the\n" \
-    "terms of the New BSD License. See <http://susam.in/licenses/bsd/>\n" \
-    "for the complete license."
+  "This is free software. You are permitted to redistribute and use it in\n" \
+  "source and binary forms, with or without modification, under the terms\n" \
+  "of the Simplified BSD License. See <http://susam.in/licenses/bsd/> for\n" \
+  "the complete license."
 
 #define STACK_SIZE          1000  /* Default loop stack size */
 #define STACK_GROWTH_FACTOR 0.1   /* How much stack to add when its full */
