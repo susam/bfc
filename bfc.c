696e16e65a1106b1750abba19aeec0acebe679fc
Susam Pal <susam@susam.in>
2019-07-29 15:05:23 +0530

    Switch to MIT license

--- bfc.c
+++ bfc.c
@@ -1,48 +1,59 @@
-/* Brainfuck compiler */
+/**
+@file
+@mainpage
 
-/*
- * Copyright (c) 2008 Susam Pal
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *     1. Redistributions of source code must retain the above copyright
- *        notice, this list of conditions and the following disclaimer.
- *     2. Redistributions in binary form must reproduce the above
- *        copyright notice, this list of conditions and the following
- *        disclaimer in the documentation and/or other materials provided
- *        with the distribution.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+Brainfuck compiler and interpreter.
+
+The MIT License (MIT)
+---------------------
+Copyright (c) 2018-2018 Susam Pal
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 
-#define VERSION     "0.1"
-#define COPYRIGHT   "Copyright (c) 2008 Susam Pal"
-#define AUTHOR      "Susam Pal"
-#define LICENSE \
-  "This is free software. You are permitted to redistribute and use it in\n" \
-  "source and binary forms, with or without modification, under the terms\n" \
-  "of the Simplified BSD License. See <http://susam.in/licenses/bsd/> for\n" \
-  "the complete license."
 
-#define STACK_SIZE          1000  /* Default loop stack size */
-#define STACK_GROWTH_FACTOR 0.1   /* How much stack to add when its full */
+/** Version of the program. */
+#define VERSION     "0.2.0"
+
+/** Author of the program. */
+#define AUTHOR "Susam Pal"
+
+/** Copyright notice. */
+#define COPYRIGHT "Copyright (c) 2008-2018 " AUTHOR
+
+/** URL to a copy of the license. */
+#define LICENSE_URL "<https://github.com/susam/bfc/blob/master/LICENSE.md>"
+
+/** URL to report issues to. */
+#define SUPPORT_URL "<https://github.com/susam/bfc/issues>"
+
+/** Maximum length of command line argument that is read. */
+#define MAX_ARG_LEN 16
+
+/** Maximum allowed length of an error message. */
+#define MAX_ERR_LEN 256
 
 void compile(char *asm_filename, char *src_filename);
 char *string(char *str);
